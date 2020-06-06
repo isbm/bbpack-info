9aaa55e0dd43475512ea1ca4e975ebff686d1930
Bo Maryniuk <bo@maryniuk.net>
2020-06-06 16:12:57 +0200

    Implement basic JSON output

--- formatters/json.go
+++ formatters/json.go
@@ -1,6 +1,11 @@
 package bbpak_formatters
 
-import bbpak_paktype "github.com/isbm/bbpack-info/paktype"
+import (
+	"bytes"
+	"encoding/json"
+
+	bbpak_paktype "github.com/isbm/bbpack-info/paktype"
+)
 
 type BBPakJSONFormat struct {
 	packages map[string]*bbpak_paktype.PackageMeta
@@ -16,7 +21,20 @@ func NewBBPakJSONFormat() *BBPakJSONFormat {
 
 // Format the output to the JSON format (useful for integrations)
 func (bbp *BBPakJSONFormat) Format() string {
-	return "json is not yet implemented"
+	var out map[string]interface{} = make(map[string]interface{})
+	for pkgName := range bbp.packages {
+		pkg := bbp.packages[pkgName]
+		pkgInfo := make(map[string]interface{})
+		pkgInfo["license"] = pkg.GetPackage().ControlFile().Licence()
+		pkgInfo["version"] = pkg.GetPackage().ControlFile().Version()
+		out[pkgName] = pkgInfo
+	}
+
+	jsonData, _ := json.Marshal(out)
+	var formatted bytes.Buffer
+	json.Indent(&formatted, jsonData, "", "\t")
+
+	return formatted.String()
 }
 
 // SetPackages has been already collected and ready to format the output
