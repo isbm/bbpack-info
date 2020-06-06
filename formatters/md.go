3264a5a354a64ba9750a7204ce09d44d3bf67acb
Bo Maryniuk <bo@maryniuk.net>
2020-06-06 20:58:16 +0200

    Implelent Markdown table

--- formatters/md.go
+++ formatters/md.go
@@ -1,6 +1,9 @@
 package bbpak_formatters
 
 import (
+	"fmt"
+	"strings"
+
 	bbpak_paktype "github.com/isbm/bbpack-info/paktype"
 )
 
@@ -17,5 +20,18 @@ func NewBBPakMarkdownFormat() *BBPakMarkdownFormat {
 
 // Format the output to the Markdown table (useful for GitHub/-Lab Wikis)
 func (bbp *BBPakMarkdownFormat) Format() string {
-	return "markdown is not yet implemented"
+	var out strings.Builder
+
+	out.WriteString(fmt.Sprintf("|%s|%s|%s|%s|%s|%s|\n", "Num", "Name", "Version", "Licence", "Size", "Maintainer"))
+	out.WriteString("|-|-|-|-|-|-|\n")
+	for idx, pkgName := range bbp.pkgNameIndex {
+		pkg := bbp.packages[pkgName]
+		out.WriteString(fmt.Sprintf("|%d|%s|%s|%s|%s|%s|\n", idx+1,
+			pkg.GetPackage().ControlFile().Package(),
+			pkg.GetPackage().ControlFile().Version(),
+			pkg.GetPackage().ControlFile().Licence(),
+			bbp.ByteIEC(pkg.GetPackage().FileSize()),
+			pkg.GetPackage().ControlFile().Maintainer()))
+	}
+	return out.String()
 }
